6c99d708f5255c1335f85be9cbc619af7ad9ca14
lpisu98 <lorenzo.pisu98@gmail.com>
2025-05-20 11:14:57 +0200

    website first version

--- python_app/app.py
+++ python_app/app.py
@@ -32,8 +32,8 @@ def home():
 
     posts, meta = get_posts(ghost_api, api_key, limit)
     return render_template("index.html", navigation=navigation, site=site, current_year=2024, posts=posts, meta=meta, 
-                           page_url_next=f'/pages/{meta['pagination']['next']}', 
-                           page_url_prev=f'/pages/{meta['pagination']['prev']}')
+                           page_url_next=f"/pages/{meta['pagination']['next']}", 
+                           page_url_prev=f"/pages/{meta['pagination']['prev']}")
 
 @app.route("/pages/<int:num>")
 def pages(num):
@@ -44,11 +44,11 @@ def pages(num):
     posts, meta = get_posts(ghost_api, api_key, limit, page=num)
     if meta['pagination']['prev'] == 1:
         return render_template("index.html", navigation=navigation, site=site, current_year=2024, posts=posts, meta=meta, 
-                           page_url_next=f'/pages/{meta['pagination']['next']}',
+                           page_url_next=f"/pages/{meta['pagination']['next']}",
                            page_url_prev=f'/')
     return render_template("index.html", navigation=navigation, site=site, current_year=2024, posts=posts, meta=meta, 
-                           page_url_next=f'/pages/{meta['pagination']['next']}',
-                           page_url_prev=f'/pages/{meta['pagination']['prev']}')
+                           page_url_next=f"/pages/{meta['pagination']['next']}",
+                           page_url_prev=f"/pages/{meta['pagination']['prev']}")
 
 @app.route("/posts/<id>")
 def post(id):
@@ -70,4 +70,4 @@ def about():
     return render_template("about.html", site=site, navigation=navigation, current_year=2024, members=members, num=num)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
